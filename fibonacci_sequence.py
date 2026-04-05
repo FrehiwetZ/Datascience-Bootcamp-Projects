74fe409d09de84e0aa99b80b1413302e45f557a9
FrehiwetZ <papsye30@gmail.com>
2026-04-05 11:41:36 -0900

    making a  fibonnaci sequence user definition

---
+++ fibonacci_sequence.py
@@ -0,0 +1,16 @@
+#define the function to generate the fibonnaco sequence
+def fibonacci(n):
+    #convert input to integer 
+    n=int(n)
+
+    #First two numbers
+    a=0
+    b=0
+
+    for i in range(n):
+        print(a, end=" ")
+        #update the values of a and b
+        a,b= b ,a+b 
+        
+#Example
+fibonacci(10)
