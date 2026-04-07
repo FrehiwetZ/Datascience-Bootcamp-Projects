150f2d5c19f460df9c86e7e28a17fa34f1911738
FrehiwetZ <papsye30@gmail.com>
2026-04-06 22:53:27 -0900

    Constructure for get_mode

--- Statistical Thinking/statistical_engine/scr/stat_engine.py
+++ Statistical Thinking/statistical_engine/scr/stat_engine.py
@@ -12,7 +12,7 @@ class StatEngine:
             
         self.data=cleaned
         self.n = len(cleaned)
-
+# for the centeral tendency 
     def get_mean(self):
         return sum(self.data) / self.n
     
@@ -24,4 +24,16 @@ class StatEngine:
         else:
             return sorted_data[mid]
         
+    def get_mode(self):
+        freq = {}
+        for x in self.data:
+            freq[x] =freq.get(x, 0) + 1
+
+        max_freq = max(freq.values())
+
+        if max_freq == 1:
+            return "No mode "
+        
+        modes = [k for k, v in freq.items() if v == max_freq]
+        return modes
     
