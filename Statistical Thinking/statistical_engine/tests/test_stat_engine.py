97c5d17f7d87d0bed0e40c6b86029ebdd75dc9a5
FrehiwetZ <papsye30@gmail.com>
2026-04-06 23:15:36 -0900

    constructure for test_mean function

--- Statistical Thinking/statistical_engine/tests/test_stat_engine.py
+++ Statistical Thinking/statistical_engine/tests/test_stat_engine.py
@@ -4,4 +4,8 @@ class TestStatEngine(unittest.TestCase):
         self.data = [1, 2, 3, 4, 5]
         self.engine = StatEngine(self.data)
     
+    def test_mean(self):
+        self.assertEqual(self.engine.get_mean(), 3)
+
+    
     
