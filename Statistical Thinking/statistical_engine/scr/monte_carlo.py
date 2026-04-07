fe6a6bc80e96549ec28921aa04c5602f98f6d897
FrehiwetZ <papsye30@gmail.com>
2026-04-06 23:12:05 -0900

    give user defined function for crash proabaility and run simulations

---
+++ Statistical Thinking/statistical_engine/scr/monte_carlo.py
@@ -0,0 +1,16 @@
+import random
+# for crash probability
+def simulate_crashes(days):
+    crash_probability = 0.045
+    crashes = 0
+
+    for _ in range(days):
+        if random.random() < crash_probaility:
+            crashes +=1 
+    return crashes / days
+#for diplaying the results of the simulation
+def run_simulation():
+    for days in [10,100,10000]:
+        prob = simulate_crashes(days)
+        print(f"Days: {days}, Simulated Crash Probability: {prob}")
+
