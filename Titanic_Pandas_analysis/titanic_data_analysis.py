f784ecec400e24c71c141314d42702bbc2c5c693
FrehiwetZ <papsye30@gmail.com>
2026-04-08 13:24:41 -0900

    exploration of a dataset

--- Titanic_Pandas_analysis/titanic_data_analysis.py
+++ Titanic_Pandas_analysis/titanic_data_analysis.py
@@ -2,3 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("titanic.csv")
 
+#Exploration of the dataset
+
+print("First 5 rows:")
+print(df.head())
+
+print("\nDataset Info:")
+print(df.info())
+
+print("\nStatistical Summary:")
+print(df.describe())
