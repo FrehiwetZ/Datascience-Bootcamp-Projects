8237bfa6ca2ec6a677644f9ea428788f0a8346be
FrehiwetZ <papsye30@gmail.com>
2026-04-08 20:58:05 -0900

    created different group so it can be suitable for data analysis

--- Titanic_Pandas_analysis/titanic_data_analysis.py
+++ Titanic_Pandas_analysis/titanic_data_analysis.py
@@ -23,5 +23,26 @@ df.drop(columns=["Cabin"], inplace=True)
 
 df.drop_duplicates(inplace = True)
 
+#Data Analysis
 
-                          
+survival_gender = df.groupby("Sex")["Survived"].mean()
+print("\nSurvival Rate By Gender:")
+print(survival_gender)
+
+survival_class= df.groupby("Pclass")["Survived"].mean()
+print("\nSurvival Rate By Passenger Class:")
+print(survival_class)
+
+survival_age = df.groupby("Pclass")["Age"].mean()
+print("\nAverage Age By Passenger Class:")
+print(survival_age)
+
+df["AgeGroup"] = pd.cut(
+    df["Age"],
+    bins=[0 ,12, 18, 35, 60, 100], 
+    labels=["Child", "Young", "Adult", "Senior"])
+
+survival_age_group = df.groupby("AgeGroup")["Survived"].mean()
+print("\nSurvival Rate By Age Group:")
+print(survival_age_group)
+
